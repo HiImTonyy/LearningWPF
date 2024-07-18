dd8ef80e8cf318d737e5aefe44a546b7916cb4f2
HiImTonyy <129976747+HiImTonyy@users.noreply.github.com>
2024-07-18 02:38:04 -0400

    Created the main function of the game. if you guess wrong, text says wrong, guess right and text says win.

--- Button11_Simon.xaml.cs
+++ Button11_Simon.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,77 @@ namespace LearningWPF
         public Button11_Simon()
         {
             InitializeComponent();
+            RoundStart();
+        }
+
+        public int counter = 1;
+        public int round = 1;
+        public List<int> lightedUpButtons = new List<int>();
+        public List<int> userSelectedButtons = new List<int>();
+
+        public async void RoundStart()
+        {
+            while (counter > 0)
+            {
+                await Task.Delay(1500);
+                Random roll = new Random();
+                int number = roll.Next(1, 5);
+
+                switch (number)
+                {
+                    case 1:
+                        topButton.Background = Brushes.LightYellow;
+                        await Task.Delay(850);
+                        topButton.Background = Brushes.Yellow;
+                        lightedUpButtons.Add(1);
+                        counter--;
+                        break;
+                    case 2:
+                        rightButton.Background = Brushes.LightGreen;
+                        await Task.Delay(850);
+                        rightButton.Background = Brushes.Green;
+                        lightedUpButtons.Add(2);
+                        counter--;
+                        break;
+                    case 3:
+                        bottomButton.Background = Brushes.Pink;
+                        await Task.Delay(850);
+                        bottomButton.Background = Brushes.HotPink;
+                        lightedUpButtons.Add(3);
+                        counter--;
+                        break;
+                    case 4:
+                        leftButton.Background = Brushes.LightBlue;
+                        await Task.Delay(850);
+                        leftButton.Background = Brushes.Blue;
+                        lightedUpButtons.Add(4);
+                        counter--;
+                        break;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+
+            switch (button.Name)
+            {
+                case "topButton":
+                    userSelectedButtons.Add(1);
+                    break;
+                case "rightButton":
+                    userSelectedButtons.Add(2);
+                    break;
+                case "bottomButton":
+                    userSelectedButtons.Add(3);
+                    break;
+                case "leftButton":
+                    userSelectedButtons.Add(4);
+                    break;
+            }
+
+            if (userSelectedButtons.SequenceEqual(lightedUpButtons)) { textt.Text = "win"; } else { textt.Text = "lose"; } 
         }
     }
 }
